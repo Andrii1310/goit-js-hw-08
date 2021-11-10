8806c0ba98420f41182959f208a1938474da4d30
Andrii Miroshnichenko <andy.miroshnichenko@gmail.com>
2021-11-10 13:39:17 +0200

    Done hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
 const galleryContainer = document.querySelector('.gallery');
 
 // Create gallery.
 const createItem = galleryItems
   .map(
     el =>
-      `<li>
+      `<div class="gallery__list">
      <a class="gallery__item" href="${el.original}">
      <img class="gallery__image"
           src="${el.preview}"
           alt="${el.description}"
       />
       </a>
-    </li>`,
+    </div>`,
   )
   .join('');
 
 galleryContainer.insertAdjacentHTML('beforeend', createItem);
 
-// On SimpleLightbox.
+// Create SimpleLightbox
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
   captionPosition: 'bottom',
 });
-
-// Описан в документации
-import SimpleLightbox from 'simplelightbox';
-// Дополнительный импорт стилей
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-
 
 console.log(galleryItems);
